4936c438fd33d137e571ca8e436478b5b6b0680d
heds <he77505357@163.com>
2023-03-07 23:28:25 +0800

    使用 git-clone 拉代码

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 #!/usr/bin/env node
 
 import inquirer from 'inquirer';
+
+
+
+import gitClone from 'git-clone';
+
 import ora from 'ora';
 import chalk from 'chalk';
 import { program }from 'commander';
-import gitDownload from 'download-git-repo';
-
-
-// inquirer.prompt([
-//   {
-//     name:'type',
-//     type: 'list',
-//     message: 'choose a type of project to init',
-//     choices: ['react', 'vue', 'h5'],
-//     default: 'react',
-//   }
-// ]).then((res) => {})
 
 
 program
@@ -26,23 +19,37 @@ program
         console.log(chalk.red('need a project name'));
         return
       }
-      const spinner = ora('template Downloading');
-      spinner.start();
 
-      //const url = 'direct:' + 'https://gitee.com/snowzoro/cli-test.git' npm install child_process
-      const url = 'direct:' + 'https://github.com/HE77XTL/template-admin-vue.git#main'
-
-      // git clone https://gitee.com/snowzoro/dodoui-react.git  download-git-repo Response code 403 (Forbidden)
-
-      gitDownload(url, name, function (err) {
-        if(err){
-          spinner.fail();
-          console.log(chalk.red('downloading template fail'));
-          console.log(chalk.gray(err));
-          return
+      inquirer.prompt([
+        {
+          name:'type',
+          type: 'list',
+          message: 'choose a type of project to init',
+          choices: ['react', 'vue', 'h5'],
+          default: 'react',
         }
-        spinner.succeed();
-        console.log(chalk.green('init project success! '));
+      ]).then((res) => {
+        console.log('res---');
+        console.log(res);
+
+        const spinner = ora('template Downloading');
+        spinner.start();
+
+        const url = 'https://gitee.com/snowzoro/cli-test.git'
+        //const url = 'https://github.com/HE77XTL/template-admin-vue.git#main'
+
+        // git clone https://gitee.com/snowzoro/dodoui-react.git  download-git-repo Response code 403 (Forbidden)
+        gitClone(url, 'test/temp2', function (err) {
+          console.log('git clone');
+          if(err){
+            spinner.fail();
+            console.log(chalk.red('downloading template fail'));
+            console.log(chalk.gray(err));
+            return
+          }
+          spinner.succeed();
+          console.log(chalk.green('init project success! '));
+        })
       })
     })
     .version('0.8.0');
